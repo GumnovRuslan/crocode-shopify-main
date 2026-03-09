14ec96d47746b2bd95db75f0611348068a42b68b
Daria <dashabry15@gmail.com>
2026-03-09 13:14:38 +0300

    Updated writing styles and Hero

--- crocode-shopify/src/components/pages/service-detail/index.tsx
+++ crocode-shopify/src/components/pages/service-detail/index.tsx
@@ -34,7 +34,6 @@ const ServiceDetailPage = ({ slug, service, projects }: TProps) => {
         }}
         title={service.title}
         subtitle={service.heroSubtitle || ""}
-        shift={true}
         animatedBg
       />
 
